9860c1238a6a784c05db8acdac47f74c4791970d
przemekNachel <przemeknachel@gmail.com>
2017-12-14 21:41:18 +0100

    Add modules and reading/saving file

---
+++ DAO.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DAO{
+
+    private String string;
+    private String filename = "countries_and_capitals.txt";
+
+    private String fileToString(String filename){
+
+        try {
+            String string = new String(Files.readAllBytes(Paths.get(filename)));
+        } catch(IOException e) {
+            System.out.println("No file " + filename);
+        }
+
+        return string;
+    }
+
+    private void stringToFile(String string, String filename){
+        try {
+            Files.write(Paths.get(filename), string.toString().getBytes());
+        } catch(IOException e) {
+            System.out.println("No file " + filename);
+        }
+    }
+}
